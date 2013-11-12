d2dfeae87edf2525b32b1cb76fa8909af1f9bfae
Striddle <Striddle@hotmail.com>
2013-11-12 08:22:57 -0500

    I hope so. it would suck if it didn't.

--- PizzaGuy/PizzaGuy/PizzaGuy/Sprite.cs
+++ PizzaGuy/PizzaGuy/PizzaGuy/Sprite.cs
@@ -1,7 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
 
 namespace PizzaGuy
 {
@@ -12,7 +19,7 @@ namespace PizzaGuy
             Vector2 Location,
             Texture2D texture,
             Rectangle initialframe,
-            Vector2 velocity)
+            Vector2 velocity);
 
     }
 }
