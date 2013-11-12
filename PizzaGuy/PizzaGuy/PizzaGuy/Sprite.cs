eb7b533774ab2cf363e2649ce56d7ecb7269eb7b
Striddle <Striddle@hotmail.com>
2013-11-12 08:21:20 -0500

    is this working?

---
+++ PizzaGuy/PizzaGuy/PizzaGuy/Sprite.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PizzaGuy
+{
+    class Sprite
+    {
+
+        public Sprite(
+            Vector2 Location,
+            Texture2D texture,
+            Rectangle initialframe,
+            Vector2 velocity)
+
+    }
+}
